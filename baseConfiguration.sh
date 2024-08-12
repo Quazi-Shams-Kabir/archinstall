2969bad71e45db4074b946c79d7040fa0c5b80eb
Quazi Shams Kabir <quaziraj12@gmail.com>
2024-08-12 13:09:23 +0530

    removed reflector and linux-zen initramfs; added firewall config and move preload to home

--- baseConfiguration.sh
+++ baseConfiguration.sh
@@ -23,6 +23,7 @@ echo "127.0.1.1 $hostname.localdomain $hostname" >> /etc/hosts
 
 ### Configure pacman
 echo "Uncomment Color, VerbosePkgLists, ParallelDownloads and add ILoveCandy"
+echo "Don't uncomment ParallelDownloads if internet is slow or unstable, it'll break whole installation"
 read -p "Press any key to continue"
 nvim /etc/pacman.conf
 
@@ -33,13 +34,12 @@ pacman -Syy
 # copied from https://gitlab.com/eflinux/arch-basic/-/blob/master/base-uefi.sh?ref_type=heads
 # if installing on a laptop, also install tlp
 # will see if I need these avahi nss-mdns
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync man-db htop fastfetch pacman-contrib grub-btrfs xdg-user-dirs xdg-utils duf inxi eza xwaylandvideobridge gnome-disk-utility firewalld os-prober ntfs-3g intel-media-driver libva-mesa-driver ttf-jetbrains-mono-nerd noto-fonts-emoji noto-fonts sof-firmware inetutils reflector curl wget
+pacman --noconfirm -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync man-db htop fastfetch pacman-contrib grub-btrfs xdg-user-dirs xdg-utils duf inxi eza xwaylandvideobridge gnome-disk-utility firewalld os-prober ntfs-3g intel-media-driver libva-mesa-driver ttf-jetbrains-mono-nerd noto-fonts-emoji noto-fonts sof-firmware inetutils curl wget
 
 ### Enable Services
 systemctl enable NetworkManager
 systemctl enable fstrim.timer # for ssd
 systemctl enable firewalld
-systemctl enable reflector.timer
 # systemctl enable avahi-daemon
 # systemctl enable tlp # my current laptop battery is dead
 
@@ -60,14 +60,12 @@ echo "After:  MODULES=(btrfs)"
 read -p "Press any key to continue"
 nvim /etc/mkinitcpio.conf
 mkinitcpio -p linux
-mkinitcpio -p linux-zen
 
 ### Set Root Password
 echo "Set root password"
 passwd
 
 ### Add User
-echo "Add user $username"
 useradd -mG wheel $username
 passwd $username
 usermod -c "$fullname" $username
@@ -82,12 +80,18 @@ read -p "Open sudoers now?" c
 EDITOR=nvim sudo -E visudo
 usermod -aG wheel $username
 
+### Firewall config
+firewall-cmd --add-port=1025-65535/tcp --permanent
+firewall-cmd --add-port=1025-65535/udp --permanent
+firewall-cmd --reload
+
 ### Move all the scripts to home dir
 mv /archinstall/zram.sh /home/$username
 mv /archinstall/paru.sh /home/$username
 mv /archinstall/yay.sh /home/$username
 mv /archinstall/timeshift.sh /home/$username
 mv /archinstall/snapshot.sh /home/$username
+mv /archinstall/preload.sh /home/$username
 
 clear
 printf "\e[1;32mDone! Type exit if prompt is '[root@archiso /]' instead of 'root@archiso ~', umount -R /mnt and reboot.\e[0m"
